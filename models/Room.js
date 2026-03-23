4b526a653e26e7f392ec9be9a9bd1cbe17b91bcb
Man <pones0050@gmail.com>
2026-03-23 20:54:59 +0700

    added rating

--- models/Room.js
+++ models/Room.js
@@ -49,6 +49,16 @@ const RoomSchema = new mongoose.Schema({
         type: String,
         required: [true, 'Please add a region']
     },
+    averageRating: {
+    type: Number,
+    min: 0,
+    max: 5,
+    default: 0
+    },
+    ratingCount: {
+        type: Number,
+        default: 0
+    },
     
 }, {
     toJSON: {virtuals: true},
