9020173ccb21043d6df5b7bbb271ff2ef6c8df5f
Man <pones0050@gmail.com>
2026-03-23 12:26:52 +0700

    add user info in reservation response

--- controllers/reservations.js
+++ controllers/reservations.js
@@ -1,34 +1,43 @@
 const Reservation = require('../models/Reservation');
 const Room = require('../models/Room');
 
-exports.getReservations = async (req,res,next) => {
+exports.getReservations = async (req, res, next) => {
     let query;
 
     console.log(req.user);
 
-    if(req.user.role !== 'admin'){
-        query = Reservation.find({user: req.user.id}).populate({
+    if (req.user.role !== 'admin') {
+        query = Reservation.find({ user: req.user.id }).populate({
             path: 'room',
             select: 'name province tel picture'
-        });
+        }).populate({
+            path: 'user',
+            select: 'name email tel'
+        });;
     } else {
-        if(req.params.roomId){
+        if (req.params.roomId) {
             console.log(req.params.roomId);
 
-            query = Reservation.find({room: req.params.roomId}).populate({
-            path: 'room',
-            select: 'name province tel picture'
+            query = Reservation.find({ room: req.params.roomId }).populate({
+                path: 'room',
+                select: 'name province tel picture'
+            }).populate({
+                path: 'user',
+                select: 'name email tel'
             });
         }
-        else{
+        else {
             query = Reservation.find().populate({
-            path: 'room',
-            select: 'name province tel picture'
+                path: 'room',
+                select: 'name province tel picture'
+            }).populate({
+                path: 'user',
+                select: 'name email tel'
             });
         }
     }
 
-    try{
+    try {
         const reservations = await query;
 
         res.status(200).json({
@@ -45,14 +54,17 @@ exports.getReservations = async (req,res,next) => {
     }
 }
 
-exports.getReservation = async (req,res,next) => {
-    try{
+exports.getReservation = async (req, res, next) => {
+    try {
         const reservations = await Reservation.findById(req.params.id).populate({
             path: 'room',
             select: 'name description tel picture'
-        });
+        }).populate({
+            path: 'user',
+            select: 'name email tel'
+        });;
 
-        if(!reservations){
+        if (!reservations) {
             console.log(err.stack);
             return res.status(404).json({
                 success: false,
@@ -73,22 +85,22 @@ exports.getReservation = async (req,res,next) => {
     }
 }
 
-exports.addReservation = async (req,res,next) => {
-    try{
+exports.addReservation = async (req, res, next) => {
+    try {
         req.body.room = req.params.roomId;
 
         req.body.user = req.user.id;
 
         const room = await Room.findById(req.params.roomId);
 
-        if(!room){
-            return res.status(404).json({success:false, message: `No room with the id of ${req.params.roomId}`});
+        if (!room) {
+            return res.status(404).json({ success: false, message: `No room with the id of ${req.params.roomId}` });
         }
 
-        const existedReservations = await Reservation.find({user:req.user.id});
+        const existedReservations = await Reservation.find({ user: req.user.id });
 
-        if(existedReservations.length >= 3 && req.user.role !== 'admin'){
-            return res.status(400).json({success:false, message: `The user with the ID of ${req.params.roomId} has already made 3 Reservations`});
+        if (existedReservations.length >= 3 && req.user.role !== 'admin') {
+            return res.status(400).json({ success: false, message: `The user with the ID of ${req.params.roomId} has already made 3 Reservations` });
         }
 
         const reservation = await Reservation.create(req.body);
@@ -107,16 +119,16 @@ exports.addReservation = async (req,res,next) => {
     }
 }
 
-exports.updateReservation = async (req,res,next) => {
-    try{
+exports.updateReservation = async (req, res, next) => {
+    try {
         let reservation = await Reservation.findById(req.params.id);
 
-        if(!reservation){
-            return res.status(404).json({success:true, message: `No reservation with the id of ${req.params.id}`});
+        if (!reservation) {
+            return res.status(404).json({ success: true, message: `No reservation with the id of ${req.params.id}` });
         }
 
-        if(reservation.user.toString() !== req.user.id && req.user.role !== 'admin'){
-            return res.status(401).json({success:false, message: `User ${req.user.id} is not authorized to update this reservation`});
+        if (reservation.user.toString() !== req.user.id && req.user.role !== 'admin') {
+            return res.status(401).json({ success: false, message: `User ${req.user.id} is not authorized to update this reservation` });
         }
 
         reservation = await Reservation.findByIdAndUpdate(req.params.id, req.body, {
@@ -138,16 +150,16 @@ exports.updateReservation = async (req,res,next) => {
     }
 }
 
-exports.deleteReservation = async (req,res,next) => {
-    try{
+exports.deleteReservation = async (req, res, next) => {
+    try {
         let reservation = await Reservation.findById(req.params.id);
 
-        if(!reservation){
-            return res.status(404).json({success:true, message: `No reservation with the id of ${req.params.id}`});
+        if (!reservation) {
+            return res.status(404).json({ success: true, message: `No reservation with the id of ${req.params.id}` });
         }
 
-        if(reservation.user.toString() !== req.user.id && req.user.role !== 'admin'){
-            return res.status(401).json({success:false, message: `User ${req.user.id} is not authorized to delete this reservation`});
+        if (reservation.user.toString() !== req.user.id && req.user.role !== 'admin') {
+            return res.status(401).json({ success: false, message: `User ${req.user.id} is not authorized to delete this reservation` });
         }
 
         await reservation.deleteOne();
